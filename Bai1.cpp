576f4e9b60d88aea40016150cda21f50de7fd390
phantoai01 <phantoai01@gmail.com>
2021-08-22 11:07:30 +0700

    save code

---
+++ Bai1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+         bool Check(int nam) {
+        return ((nam % 4 == 0 && nam % 100 != 0) || nam % 400 == 0);
+        }
+int main(){
+        int a;
+        cout << "Nhap gia tri cua a = " ;
+        cin >> a;
+        if(a > 0){
+                cout << a << " la so duong"<< endl;
+        }else if(a < 0){
+                cout << a << " la so am"<< endl;
+        }else{
+                cout << "a = " << 0;
+        }
+        return 0;
+}
